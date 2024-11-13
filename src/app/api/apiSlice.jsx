b31f517403f335b14fc1b4d76bb2ff67d2f4405b
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2024-11-13 13:12:50 -0800

    baseUrl changed

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -1,14 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://threadcraft-ecommerce.onrender.com/api'
-    : 'http://localhost:3500/api'
+// const baseUrl =
+//   process.env.NODE_ENV === 'production'
+//     ? 'https://threadcraft-ecommerce.onrender.com'
+//     : 'http://localhost:3500/api'
 
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl,
+    baseUrl: 'https://threadcraft-ecommerce.onrender.com/api',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token
 
